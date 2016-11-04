3b4b728ffe04e36131a455cfdc3d3e40d763139c
PoroCYon <porocyon@openmailbox.org>
2016-11-04 22:10:10 +0000

    make install_all.sh execute 'npm install' and change PaX flags (when available) so it doesn't blurt mprotect errors all over

--- install_all.sh
+++ install_all.sh
@@ -1,2 +1,12 @@
+#!/usr/bin/env bash
+
+ZIF_BIN="$GOPATH/bin/zifd"
+
 go install
-cd zifd && go install
+pushd zifd; go install; popd
+pushd ui;  npm install; popd
+
+if [ -d "/proc/sys/kernel/pax" ]; then
+    paxctl -c "$ZIF_BIN" && setfattr -n user.pax.flags -v "emr" "$ZIF_BIN"
+fi
+
