97a3c8fe3740b4830fdea789e08bdad183034ac7
PoroCYon <porocyon@openmailbox.org>
2016-11-22 20:02:27 +0000

    integrate command.go into httpserver.go (very unfinished)

--- command.go
+++ command.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"io"
 	"os"
 	"strings"
 
@@ -14,50 +15,50 @@ import (
 // Command input types
 
 type CommandPeer struct {
-	address string
+    Address string `json:"address"`
 }
 
 type CommandPing     CommandPeer
 type CommandAnnounce CommandPeer
 type CommandRSearch struct {
 	CommandPeer
-	query   string
-	page    int
+    Query   string `json:"query"`
+    Page    int    `json:"page"`
 }
 type CommandPeerSearch CommandRSearch
 type CommandPeerRecent struct {
 	CommandPeer
-	page int
+    Page int `json:"page"`
 }
 type CommandPeerPopular CommandPeerRecent
 type CommandMirror      CommandPeer
 type CommandPeerIndex struct {
 	CommandPeer
-	since int
+    Since int `json:"since"`
 }
 
 type CommandMeta struct {
-	pid int
-	key string
+    PId int    `json:"pid"`
+    Key string `json:"key"`
 }
 
 type CommandAddPost Post
 type CommandSelfIndex struct {
-	since int
+    Since int `json:"since"`
 }
 type CommandResolve   CommandPeer
 type CommandBootstrap CommandPeer
 type CommandSelfSearch struct {
-	query string
-	page  int
+    Query string `json:"query"`
+    Page  int    `json:"page"`
 }
 type CommandSelfRecent struct {
-	page int
+    Page int `json:"page"`
 }
 type CommandSelfPopular CommandSelfRecent
 type CommandAddMeta struct {
 	CommandMeta
-	value string
+    Value string `json:"value"`
 }
 type CommandGetMeta CommandMeta
 type CommandSaveCollection    interface{}
@@ -68,40 +69,41 @@ type CommandSaveRoutingTable  interface{}
 // Command output types
 
 type CommandResult struct {
-	isOk   bool
-	result interface{}
-	errmsg error
+    IsOK   bool        `json:"status"`
+    Result interface{} `json:"value"`
+    Error  error       `json:"err"`
 }
 
 func (cr *CommandResult) WriteJSON(w io.Writer) {
 	e := json.NewEncoder(w)
 
-	if cr.isOk {
-		if cr.result == nil
+	if cr.IsOK {
+		if cr.Result == nil {
 			e.Encode(struct{
 				Status string `json:"status"`
 			}{"ok"})
-		else
+		} else {
 			e.Encode(struct{
-				Status string	  `json:"status"`
+				Status string      `json:"status"`
 				Value  interface{} `json:"value"`
-			}{"ok", cr.result})
-	}
-	else {
-		if cr.errmsg == nil
-			cr.errmsg = errors.New("An unspecified error occured.")
+			}{"ok", cr.Result})
+		}
+	} else {
+		if cr.Error == nil {
+			cr.Error = errors.New("Something bad happened, but we don't know bad, which makes the fact much worse.")
+		}
 
 		e.Encode(struct{
 			Status string `json:"status"`
 			Error  string `json:"err"`
-		}{"err", cr.errmsg.Error()})
+		}{"err", cr.Error.Error()})
 	}
 }
 
 // Command server type
 
 type CommandServer struct {
-	localPeer *LocalPeer
+	LocalPeer *LocalPeer
 }
 
 // Command functions
@@ -110,63 +112,68 @@ func (cs *CommandServer) Ping(p CommandPing) CommandResult {
 	log.Info("Command: Ping request")
 
 	// TODO: implement
-	return {true,nil,nil}
+	return CommandResult{true,nil,nil}
 }
 func (cs *CommandServer) Announce(a CommandAnnounce) CommandResult {
 	var err error
 
 	log.Info("Command: Announce request")
 
-	peer := cs.localPeer.GetPeer(a.address)
+	peer := cs.LocalPeer.GetPeer(a.Address)
 
 	if peer == nil {
-		peer, err = cs.localPeer.ConnectPeer(a.address)
+		peer, err = cs.LocalPeer.ConnectPeer(a.Address)
 
-		if err != nil
-			return {false,nil,err}
+		if err != nil {
+			return CommandResult{false,nil,err}
+		}
 	}
 
-	_, err = peer.ConnectClient(cs.localPeer)
-	if err != nil
-		return {false,nil,err}
+	_, err = peer.ConnectClient(cs.LocalPeer)
+	if err != nil {
+		return CommandResult{false,nil,err}
+	}
 
-	err = peer.Announce(cs.localPeer)
+	err = peer.Announce(cs.LocalPeer)
 
-	return {err != nil,nil,err}
+	return CommandResult{err != nil,nil,err}
 }
 func (cs *CommandServer) RSearch(rs CommandRSearch) CommandResult {
 	var err error
 
 	log.Info("Command: Peer Remote Search request");
 
-	peer = cs.localPeer.GetPeer(rs.CommandPeer.address)
+    peer := cs.LocalPeer.GetPeer(rs.CommandPeer.Address)
 
 	if peer == nil {
-		peer, err = cs.localPeer.ConnectPeer(rs.CommandPeer.address)
-		if err != nil
-			return {false,nil,err}
+		peer, err = cs.LocalPeer.ConnectPeer(rs.CommandPeer.Address)
+		if err != nil {
+			return CommandResult{false,nil,err}
+		}
 	}
 
-	posts, stream, err := peer.Search(rs.query, rs.page)
+	posts, stream, err := peer.Search(rs.Query, rs.Page)
 
-	if stream != nil
+	if stream != nil {
 		defer stream.Close()
+	}
 
-	return {err != nil,posts,err}
+	return CommandResult{err != nil,posts,err}
 }
 func (cs *CommandServer) PeerSearch(ps CommandPeerSearch) CommandResult {
 	var err error
 
 	log.Info("Command: Peer Search request")
 
-	if !cs.localPeer.Databases.Has(ps.CommandPeer.address)
-		return cs.RSearch(ps.(CommandRSearch)) // this is completely fine
+	if !cs.LocalPeer.Databases.Has(ps.CommandPeer.Address) {
+		return cs.RSearch(CommandRSearch{ps.CommandPeer,ps.Query,ps.Page})
+	}
 
-	db, _ := cs.localPeer.Databases.Get(ps.CommandPeer.address)
+	db, _ := cs.LocalPeer.Databases.Get(ps.CommandPeer.Address)
 
-	posts, err := db.(*Database).Search(ps.query, ps.page)
+	posts, err := db.(*Database).Search(ps.Query, ps.Page)
 
-	return {err != nil,posts,err}
+	return CommandResult{err != nil,posts,err}
 }
 func (cs *CommandServer) PeerRecent(pr CommandPeerRecent) CommandResult {
 	var err   error
@@ -174,25 +181,27 @@ func (cs *CommandServer) PeerRecent(pr CommandPeerRecent) CommandResult {
 
 	log.Info("Command: Peer Recent request")
 
-	if pr.CommandPeer.address == cs.localPeer.Entry.ZifAddress.Encode() {
-		posts, err = cs.localPeer.Database.Query
+	if pr.CommandPeer.Address == cs.LocalPeer.Entry.ZifAddress.Encode() {
+		posts, err = cs.LocalPeer.Database.QueryRecent(pr.Page)
 
-		return {err != nil,posts,err}
+		return CommandResult{err != nil,posts,err}
 	}
 
-	peer := cs.localPeer.GetPeer(pr.CommandPeer.address)
-	if peer == null {
-		peer, err = cs.localPeer.ConnectPeer(pr.CommandPeer.address)
-		if err != nil
-			return {false,nil,err}
+	peer := cs.LocalPeer.GetPeer(pr.CommandPeer.Address)
+	if peer == nil {
+		peer, err = cs.LocalPeer.ConnectPeer(pr.CommandPeer.Address)
+		if err != nil {
+			return CommandResult{false,nil,err}
+		}
 	}
 
-	posts, stream, err := peer.Recent(pr.page)
+	posts, stream, err := peer.Recent(pr.Page)
 
-	if stream != nil
+	if stream != nil {
 		defer stream.Close()
+	}
 
-	return {err != nil,posts,err}
+	return CommandResult{err != nil,posts,err}
 }
 func (cs *CommandServer) PeerPopular(pp CommandPeerPopular) CommandResult {
 	var err   error
@@ -200,179 +209,188 @@ func (cs *CommandServer) PeerPopular(pp CommandPeerPopular) CommandResult {
 
 	log.Info("Command: Peer Popular request")
 
-	if pp.CommandPeer.address == cs.localPeer.Entry.ZifAddress.Encode() {
-		posts, err = cs.localPeer.Database.Query
+	if pp.CommandPeer.Address == cs.LocalPeer.Entry.ZifAddress.Encode() {
+		posts, err = cs.LocalPeer.Database.QueryPopular(pp.Page)
 
-		return {err == nil,posts,err}
+		return CommandResult{err == nil,posts,err}
 	}
 
-	peer := cs.localPeer.GetPeer(pp.CommandPeer.address)
-	if peer == null {
-		peer, err = cs.localPeer.ConnectPeer(pp.CommandPeer.address)
-		if err != nil
-			return {false,nil,err}
+	peer := cs.LocalPeer.GetPeer(pp.CommandPeer.Address)
+	if peer == nil {
+		peer, err = cs.LocalPeer.ConnectPeer(pp.CommandPeer.Address)
+		if err != nil {
+			return CommandResult{false,nil,err}
+		}
 	}
 
-	posts, stream, err := peer.Popular(pp.page)
+	posts, stream, err := peer.Popular(pp.Page)
 
-	if stream != nil
+	if stream != nil {
 		defer stream.Close()
+	}
 
-	return {err == nil,posts,err}
+	return CommandResult{err == nil,posts,err}
 }
 func (cs *CommandServer) Mirror(cm CommandMirror) CommandResult {
 	var err error
 
 	log.Info("Command: Peer Mirror request")
 
-	peer := cs.localPeer.GetPeer(cm.address)
+	peer := cs.LocalPeer.GetPeer(cm.Address)
 	if peer == nil {
-		peer, err = cs.localPeer.ConnectPeer(cm.address)
-		if err != nil
-			return {false,nil,err}
+		peer, err = cs.LocalPeer.ConnectPeer(cm.Address)
+		if err != nil {
+			return CommandResult{false,nil,err}
+		}
 	}
 
 	// TODO: make this configurable
 	d := fmt.Sprintf("./data/%s", peer.ZifAddress.Encode())
-	os.Mkdir(fmt.Sprintf("./data/%s", d, 0777)
+	os.Mkdir(fmt.Sprintf("./data/%s", d), 0777)
 	db := NewDatabase(d)
 	db.Connect()
 
-	cs.localPeer.Databases.Set(peer.ZifAddress.Encode(), db)
+	cs.LocalPeer.Databases.Set(peer.ZifAddress.Encode(), db)
 
 	_, err = peer.Mirror(db)
-	if err != nil
-		return {false,nil,err}
+	if err != nil {
+		return CommandResult{false,nil,err}
+	}
 
 	// TODO: wjh: is this needed? -poro
-	cs.localPeer.Databases.Set(peer.ZifAddress.Encode(), db)
+	cs.LocalPeer.Databases.Set(peer.ZifAddress.Encode(), db)
 
-	return {true,nil,nil}
+	return CommandResult{true,nil,nil}
 }
 func (cs *CommandServer) PeerIndex(ci CommandPeerIndex) CommandResult {
 	var err error
 
 	log.Info("Command: Peer Index request")
 
-	if !cs.localPeer.Database.Has(ci.CommandPeer.address)
-		return {false,nil,errors.New("Peer database not loaded.")}
+	if !cs.LocalPeer.Databases.Has(ci.CommandPeer.Address) {
+		return CommandResult{false,nil,errors.New("Peer database not loaded.")}
+	}
 
-	db, _ := cs.localPeer.Databases.Get(ci.CommandPeer.address)
-	err = db.(*Database).GenerateFts(ci.since)
+	db, _ := cs.LocalPeer.Databases.Get(ci.CommandPeer.Address)
+	err = db.(*Database).GenerateFts(ci.Since)
 
-	return {err == nil,nil,err}
+	return CommandResult{err == nil,nil,err}
 }
 
 // self
 
-func (cs *CommandServer) AddPost(cp CommandAddPost) CommandResult {
+func (cs *CommandServer) AddPost(ap CommandAddPost) CommandResult {
 	log.Info("Command: Add Post request")
 
-	cs.localPeer.AddPost(cp, false)
+	cs.LocalPeer.AddPost(
+		Post{ap.Id,ap.InfoHash,ap.Title,ap.Size,ap.FileCount,ap.Seeders,ap.Leechers,ap.UploadDate,ap.Tags},
+		false)
 
-	return {true,nil,nil}
+	return CommandResult{true,nil,nil}
 }
 func (cs *CommandServer) SelfIndex(ci CommandSelfIndex) CommandResult {
 	log.Info("Command: FTS Index request")
 
-	err := cs.LocalPeer.Database.GenerateFts(ci.since)
+	err := cs.LocalPeer.Database.GenerateFts(ci.Since)
 
-	return {err == nil, nil, err}
+	return CommandResult{err == nil, nil, err}
 }
 func (cs *CommandServer) Bootstrap(cb CommandBootstrap) CommandResult {
 	log.Info("Command: Bootstrap request")
 
-	addrnport = strings.Split(cb.address, ":")
+	addrnport := strings.Split(cb.Address, ":")
 
-	address := addrnport[0]
+	host := addrnport[0]
 	var port string
-	if len(address) == 1
+	if len(addrnport) == 1 {
 		port = "5050" // TODO: make this configurable
-	else
+	} else {
 		port = addrnport[1]
+	}
 
-	peer, err := cs.localPeer.ConnectPeerDirect(host + ":" + port)
-	if err != nil
-		return {false,nil,err}
+	peer, err := cs.LocalPeer.ConnectPeerDirect(host + ":" + port)
+	if err != nil {
+		return CommandResult{false,nil,err}
+	}
 
-	peer.ConnectClient(cs.localPeer)
+	peer.ConnectClient(cs.LocalPeer)
 
-	_, err = peer.Bootstrap(cs.localPeer.RoutingTable)
+	_, err = peer.Bootstrap(cs.LocalPeer.RoutingTable)
 
-	return {err == nil,nil,err}
+	return CommandResult{err == nil,nil,err}
 }
-func (cs *CommandServer) SelfSearch(cs CommandSelfSearch) CommandResult {
+func (cs *CommandServer) SelfSearch(css CommandSelfSearch) CommandResult {
 	log.Info("Command: Search request")
 
-	posts, err := cs.localPeer.Database.Search(cs.query, cs.page)
+	posts, err := cs.LocalPeer.Database.Search(css.Query, css.Page)
 
-	return {err == nil,posts,err}
+	return CommandResult{err == nil,posts,err}
 }
 func (cs *CommandServer) SelfRecent(cr CommandSelfRecent) CommandResult {
 	log.Info("Command: Recent request")
 
-	posts, err := cs.localPeer.Database.QueryRecent(cs.page)
+	posts, err := cs.LocalPeer.Database.QueryRecent(cr.Page)
 
-	return {err == nil,posts,err}
+	return CommandResult{err == nil,posts,err}
 }
 func (cs *CommandServer) SelfPopular(cp CommandSelfPopular) CommandResult {
 	log.Info("Command: Popular request")
 
-	posts, err := cs.localPeer.Database.QueryPopular(cp.page)
+	posts, err := cs.LocalPeer.Database.QueryPopular(cp.Page)
 
-	return {err == nil,posts,err}
+	return CommandResult{err == nil,posts,err}
 }
 func (cs *CommandServer) AddMeta(cam CommandAddMeta) CommandResult {
 	log.Info("Command: Add Meta request")
 
-	err := cs.localPeer.Database.AddMeta(cam.CommandMeta.pid, cam.CommandMeta.key, cam.value)
+	err := cs.LocalPeer.Database.AddMeta(cam.CommandMeta.PId, cam.CommandMeta.Key, cam.Value)
 
-	return {err == nil,nil,err}
+	return CommandResult{err == nil,nil,err}
 }
 func (cs *CommandServer) GetMeta(cgm CommandGetMeta) CommandResult {
 	log.Info("Command: Get Meta request")
 
-	val, err := cs.localPeer.Database.GetMeta(cgm.pid, cgm.key)
+	val, err := cs.LocalPeer.Database.GetMeta(cgm.PId, cgm.Key)
 
-	return {err == nil,nil,err}
+	return CommandResult{err == nil,val,err}
 }
 func (cs *CommandServer) SaveCollection(csc CommandSaveCollection) CommandResult {
 	log.Info("Command: Save Collection request")
 
 	// TODO: make this configurable
-	cs.localPeer.Collection.Save("./data/collection.dat")
+	cs.LocalPeer.Collection.Save("./data/collection.dat")
 
-	return {true,nil,nil}
+	return CommandResult{true,nil,nil}
 }
 func (cs *CommandServer) RebuildCollection(crc CommandRebuildCollection) CommandResult {
 	var err error
 
 	log.Info("Command: Rebuild Collection request")
 
-	cs.localPeer.Collection, err = CreateCollection(cs.localPeer.Database, 0, PieceSize)
-	return {err =! nil,nil,err}
+	cs.LocalPeer.Collection, err = CreateCollection(cs.LocalPeer.Database, 0, PieceSize)
+	return CommandResult{err != nil,nil,err}
 }
 func (cs *CommandServer) Peers(cp CommandPeers) CommandResult {
 	log.Info("Command: Peers request")
 
-	ps := make([]*Peer, cs.localPeer.Peers.Count() + 1)
+	ps := make([]*Peer, cs.LocalPeer.Peers.Count() + 1)
 
-	ps[0] = &cs.localPeer.Peer
+	ps[0] = &cs.LocalPeer.Peer
 
 	i := 1
-	for _, p := range cs.localPeer.Peers.Items() {
+	for _, p := range cs.LocalPeer.Peers.Items() {
 		ps[i] = p.(*Peer)
 		i = i + 1
 	}
 
-	return {true,ps,nil}
+	return CommandResult{true,ps,nil}
 }
 func (cs *CommandServer) SaveRoutingTable(csrt CommandSaveRoutingTable) CommandResult {
 	log.Info("Command: Save Routing Table request")
 
 	// TODO: make this configurable
-	err := cs.localPeer.RoutingTable.Save("dht")
+	err := cs.LocalPeer.RoutingTable.Save("dht")
 
-	return {err != nil,nil,err}
+	return CommandResult{err != nil,nil,err}
 }
 
